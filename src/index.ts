996d883ddd7b62944067ca50a37faba1eadadfdd
Sreeram A <sriramlibra0@gmail.com>
2023-04-22 12:08:23 +0530

    typescript crud

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,71 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+import { v4 as uuidV4 } from 'uuid';
 
-import confetti from 'canvas-confetti';
 
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+type Task = {
+  id: string;
+  title: string;
+  completed: boolean;
+  createdAt: Date;
+};
+
+
+const list = document.querySelector<HTMLUListElement>('#list');
+
+const form =
+  document.getElementById('new-task-form') as HTMLFormElement | null;
+
+const input = document.querySelector<HTMLInputElement>('#new-task-title');
+
+
+const tasks: Task[] =loadTask();
+tasks.forEach(addListItem);
+saveTasks();
+
+
+form?.addEventListener('submit', (e) => {
+  e.preventDefault();
+  if (input?.value == '' || input?.value == null) return;
+
+  const newTask: Task = {
+    id: uuidV4(),
+    title: input.value,
+    completed: false,
+    createdAt: new Date(),
+  };
+  
+  tasks.push(newTask);
+
+  addListItem(newTask);
+  
+  saveTasks();
+
+  input.value='';
+});
+
+function addListItem(task: Task) {
+  const item = document.createElement('li');
+  const label = document.createElement('label');
+  const checkBox = document.createElement('input');
+  checkBox.type = 'checkbox';
+  checkBox.checked=task.completed;
+  checkBox.addEventListener('change', () => {
+    task.completed = checkBox.checked;
+    saveTasks();
+  });
+  label.append(checkBox, task.title);
+  item.append(label);
+  list?.append(item);
+}
+
+
+
+function saveTasks() {
+  localStorage.setItem('TASKS', JSON.stringify(tasks));
+}
+
+function loadTask(): Task[] {
+  let JSONtasks = localStorage.getItem('TASKS');
+  if (JSONtasks == null) return [];
+
+  return JSON.parse(JSONtasks);
+}
